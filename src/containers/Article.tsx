66b6e32d8b6cf84f900f42f299b3791454f2585a
Shakthi Senavirathna <shakthis.mailbox@gmail.com>
2021-10-31 01:37:36 +0530

    Fixed Article

--- src/containers/Article.tsx
+++ src/containers/Article.tsx
@@ -1,31 +1,48 @@
-import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+
+import { useParams } from "react-router-dom";
+
+import { Box } from "@mui/system";
+import { Fab, Typography, Zoom } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+
 import { fetchArticleById } from "../api";
 
 // used as the structure for the article
 interface Article {
-    title: string,
-    content: string
+  title: string;
+  content: string;
 }
-
 
 const Article = () => {
-    let { articleId } = useParams<{articleId:string}>();
-    let [article, setArticle] = useState<Article>();
+  const { articleId } = useParams<{ articleId: string }>();
+  const [article, setArticle] = useState<Article>();
 
-    useEffect(() => {
-        fetchArticleById(articleId).then(response => {
-            setArticle(response.data)
-        });
-    }, [])
+  useEffect(() => {
+    fetchArticleById(articleId).then((response) => {
+      setArticle(response.data);
+    });
+  }, []);
 
-    return (
-        <>
-            <Typography variant="h4">{article?.title}</Typography>
-            {article?.content}
-        </>
-    )
-}
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" padding={4}>
+      <Box paddingBottom={16} maxWidth={400}>
+        <Typography variant="h4" align="center">
+          {article?.title}
+        </Typography>
+        <Typography variant="body1" align="center">
+          {article?.content}
+        </Typography>
+      </Box>
+      <Box position="fixed" bottom={16} right={16}>
+        <Zoom in={true}>
+          <Fab color="primary" aria-label="back" href="/">
+            <ArrowBack />
+          </Fab>
+        </Zoom>
+      </Box>
+    </Box>
+  );
+};
 
-export default Article
+export default Article;
