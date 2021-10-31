63de1f8bf3903e5ab60db8b916722221ca4ea4ed
“Sudaraka <sudarakayasindu@gmail.com>
2021-10-31 22:50:52 +0800

    Align menu items

--- src/containers/Menu.tsx
+++ src/containers/Menu.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 import { fetchMenu } from "../api";
@@ -27,22 +27,23 @@ const Menu = () => {
 
     return (
         <Box>
-            <Box
-                component="form"
-                sx={{ '& > :not(style)': { m: 1 }, display: "flex", flex: "1", justifyContent: "center" }}
-                noValidate
-            >
-                <TextField onChange={event => onEdit(event.target.value)} id="outlined-basic" label="Search" variant="outlined" />
+            <Box component="form" sx={{ '& > :not(style)': { m: 1 }, display: "flex", flexDirection: "row", justifyContent: "center" }} noValidate>
+                <TextField style={{
+                    textAlign: 'center',
+                    width: "100%",
+                    maxWidth: "55ch"
+                }}
+                    onChange={event => onEdit(event.target.value)} id="search" label="සොයන්න" variant="outlined" />
             </Box>
             <List>
                 {menuItems.map((item) => {
-                    if(searchQuery && !item.title.includes(searchQuery) && !item.id.includes(searchQuery)) {
+                    if (searchQuery && !item.title.includes(searchQuery) && !item.id.includes(searchQuery)) {
                         return null
                     }
                     return (
                         <ListItem key={item.id}>
-                            <ListItemButton component="a" href={`/article/${item.id}`}>
-                                <ListItemText primary={item.title} />
+                            <ListItemButton sx={{ textAlign: "center" }} component="a" href={`/article/${item.id}`}>
+                                <Typography flexGrow={1} align="center" variant="h5">{item.title}</Typography>
                             </ListItemButton>
                         </ListItem>
                     );
