762a2bccc0009c52b22212a5459191d942f8c582
“Sudaraka <sudarakayasindu@gmail.com>
2021-10-31 19:05:27 +0800

    Search works :fire:

--- src/containers/Menu.tsx
+++ src/containers/Menu.tsx
@@ -4,36 +4,52 @@ import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import { Box } from "@mui/system";
 
 import { fetchMenu } from "../api";
+import { TextField } from "@material-ui/core";
 
 interface MenuItem {
-  id: string;
-  title: string;
+    id: string;
+    title: string;
 }
 
 const Menu = () => {
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+    const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+    const [searchQuery, setSearchQuery] = useState<string>("");
 
-  useEffect(() => {
-    fetchMenu().then((response) => {
-      setMenuItems(response.data.menuItems);
-    });
-  }, []);
+    useEffect(() => {
+        fetchMenu().then((response) => {
+            setMenuItems(response.data.menuItems);
+        });
+    }, []);
 
-  return (
-    <Box>
-      <List>
-        {menuItems.map((item) => {
-          return (
-            <ListItem key={item.id}>
-              <ListItemButton component="a" href={`/article/${item.id}`}>
-                <ListItemText primary={item.title} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
-    </Box>
-  );
+    const onEdit = (value: string) => {
+        setSearchQuery(value);
+    };
+
+    return (
+        <Box>
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { m: 1 }, display: "flex", flex: "1", justifyContent: "center" }}
+                noValidate
+            >
+                <TextField onChange={event => onEdit(event.target.value)} id="outlined-basic" label="Search" variant="outlined" />
+            </Box>
+            <List>
+                {menuItems.map((item) => {
+                    if(searchQuery && !item.title.includes(searchQuery) && !item.id.includes(searchQuery)) {
+                        return null
+                    }
+                    return (
+                        <ListItem key={item.id}>
+                            <ListItemButton component="a" href={`/article/${item.id}`}>
+                                <ListItemText primary={item.title} />
+                            </ListItemButton>
+                        </ListItem>
+                    );
+                })}
+            </List>
+        </Box>
+    );
 };
 
 export default Menu;
