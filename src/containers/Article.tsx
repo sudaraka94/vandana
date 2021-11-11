4916aae935b7bdf1676f7bbce97cf1d88b028bb9
Shakthi Senavirathna <shakthis.mailbox@gmail.com>
2021-11-11 15:54:44 +0530

    Create article content component

--- src/containers/Article.tsx
+++ src/containers/Article.tsx
@@ -3,132 +3,160 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { Box } from "@mui/system";
-import { Backdrop, Button, CircularProgress, Fab, Typography, Zoom } from "@mui/material";
-import { Add, ArrowBack, Close, MoreHoriz, MoreVert, ZoomIn, ZoomOut } from "@mui/icons-material";
+import {
+  Backdrop,
+  Button,
+  CircularProgress,
+  Fab,
+  Typography,
+  Zoom,
+} from "@mui/material";
+import {
+  Add,
+  ArrowBack,
+  Close,
+  MoreHoriz,
+  MoreVert,
+  ZoomIn,
+  ZoomOut,
+} from "@mui/icons-material";
 
 import { fetchArticleById } from "../api";
+import ContentContainer from "../components/ContentContainer";
 
 // used as the structure for the article
 interface Suggestion {
-    id: string;
-    title: string;
+  id: string;
+  title: string;
 }
 
 interface Article {
-    title: string;
-    content: string;
-    suggestions: Suggestion[];
+  title: string;
+  content: string;
+  suggestions: Suggestion[];
 }
 
 const Article = () => {
-    const { articleId } = useParams<{ articleId: string }>();
-    const [article, setArticle] = useState<Article>();
-    const [fontSize, setFontSize] = useState<number>(1);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [showBtns, setShowBtns] = useState<boolean>(false);
+  const { articleId } = useParams<{ articleId: string }>();
+  const [article, setArticle] = useState<Article>();
+  const [fontSize, setFontSize] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [showBtns, setShowBtns] = useState<boolean>(false);
 
-    useEffect(() => {
-        fetchArticleById(articleId).then((response) => {
-            setArticle(response.data);
-        })
-            .finally(() => {
-                setIsLoading(false);
-            })
-    }, []);
+  useEffect(() => {
+    fetchArticleById(articleId)
+      .then((response) => {
+        setArticle(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    const zoomIn = () => {
-        if (fontSize < 16) {
-            setFontSize(fontSize * 1.1);
-        }
+  const zoomIn = () => {
+    if (fontSize < 16) {
+      setFontSize(fontSize * 1.1);
     }
+  };
 
-    const zoomOut = () => {
-        if (fontSize > 1) {
-            setFontSize(fontSize * 0.9);
-        }
+  const zoomOut = () => {
+    if (fontSize > 1) {
+      setFontSize(fontSize * 0.9);
     }
+  };
 
-    const toggleShowBtns = () => {
-        setShowBtns(!showBtns);
-    }
+  const toggleShowBtns = () => {
+    setShowBtns(!showBtns);
+  };
 
-    return (
-        <>
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={isLoading}
+  return (
+    <>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        padding={4}
+      >
+        <Box width={"100%"} maxWidth={800}>
+          <Typography variant="h4" align="center">
+            {article?.title}
+          </Typography>
+          <ContentContainer article={article?.content} fontSize={fontSize} />
+        </Box>
+        <Box position="fixed" bottom={16} right={16}>
+          <Zoom in={true}>
+            <Fab
+              color="primary"
+              aria-label="back"
+              onClick={() => {
+                toggleShowBtns();
+              }}
             >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-            <Box display="flex" flexDirection="column" alignItems="center" padding={4}>
-                <Box width={"100%"} maxWidth={800}>
-                    <Typography variant="h4" align="center">
-                        {article?.title}
-                    </Typography>
-                    <div dangerouslySetInnerHTML={{ __html: article?.content || "" }} style={{ fontSize: `${fontSize}em` }}>
-                    </div>
-                </Box>
-                <Box position="fixed" bottom={16} right={16}>
-                    <Zoom in={true}>
-                        <Fab color="primary" aria-label="back" onClick={() => { toggleShowBtns() }}>
-                            {
-                                showBtns ?
-                                    <MoreHoriz />
-                                    :
-                                    <MoreVert />
-                            }
+              {showBtns ? <MoreHoriz /> : <MoreVert />}
+            </Fab>
+          </Zoom>
+        </Box>
+        <Box display="flex" flexDirection="row" alignItems="center">
+          {article?.suggestions?.map((suggestion) => {
+            return (
+              <Button
+                sx={{ margin: "3px" }}
+                key={suggestion.id}
+                variant="outlined"
+                href={`/article/${suggestion.id}`}
+              >
+                {suggestion.title}
+              </Button>
+            );
+          })}
+        </Box>
 
-                        </Fab>
-                    </Zoom>
-                </Box>
-                <Box display="flex" flexDirection="row" alignItems="center">
-                    {
-                        article?.suggestions?.map(
-                            suggestion => {
-                                return (
-                                    <Button sx={{ margin: "3px" }} key={suggestion.id} variant="outlined" href={`/article/${suggestion.id}`}>
-                                        {suggestion.title}
-                                    </Button>
-                                );
-                            }
-                        )
-
-                    }
-                </Box>
-
-                {
-                    showBtns ?
-                        (
-                            <>
-                                <Box position="fixed" bottom={90} right={16}>
-                                    <Zoom in={true}>
-                                        <Fab color="primary" aria-label="back" href="/">
-                                            <ArrowBack />
-                                        </Fab>
-                                    </Zoom>
-                                </Box>
-                                <Box position="fixed" bottom={164} right={16}>
-                                    <Zoom in={true}>
-                                        <Fab color="primary" aria-label="out" onClick={event => { zoomOut() }}>
-                                            <ZoomOut />
-                                        </Fab>
-                                    </Zoom>
-                                </Box>
-                                <Box position="fixed" bottom={238} right={16}>
-                                    <Zoom in={true}>
-                                        <Fab color="primary" aria-label="in" onClick={event => { zoomIn() }}>
-                                            <ZoomIn />
-                                        </Fab>
-                                    </Zoom>
-                                </Box>
-                            </>
-                        )
-                        :
-                        ""
-                }
+        {showBtns && (
+          <>
+            <Box position="fixed" bottom={90} right={16}>
+              <Zoom in={true}>
+                <Fab color="primary" aria-label="back" href="/">
+                  <ArrowBack />
+                </Fab>
+              </Zoom>
             </Box>
-        </>
-    );
+            <Box position="fixed" bottom={164} right={16}>
+              <Zoom in={true}>
+                <Fab
+                  color="primary"
+                  aria-label="out"
+                  onClick={(event) => {
+                    zoomOut();
+                  }}
+                >
+                  <ZoomOut />
+                </Fab>
+              </Zoom>
+            </Box>
+            <Box position="fixed" bottom={238} right={16}>
+              <Zoom in={true}>
+                <Fab
+                  color="primary"
+                  aria-label="in"
+                  onClick={(event) => {
+                    zoomIn();
+                  }}
+                >
+                  <ZoomIn />
+                </Fab>
+              </Zoom>
+            </Box>
+          </>
+        )}
+      </Box>
+    </>
+  );
 };
 
 export default Article;
